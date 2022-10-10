f4f10db44e6aa8375618720e497224326fbcd47d
ombiro96 <bakutarb@gmail.com>
2022-10-10 15:50:26 +0300

    Will continue later

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("hello?")
+// alert("hello?")
+const quotes=document.querySelector(".quotes");
+const author=document.querySelector(".persons");
+
+function generate(){
+    quotes.innerText="\"Mwenda is such an enticing name\""
+    author.innerText="by Clifford Ombiro"
+}
